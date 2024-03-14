97be1b9a052c4f440d4602244976b4727e118638
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-14 20:09:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+
+
+
 from math import sqrt # Importando a biblioteca math, em específico o SQRT para a dar a raiz quadrada
+
+print("-----------------------------------------------")
+print("|  PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU  |" )
+print("-----------------------------------------------")
+print("") # espaços
+
 

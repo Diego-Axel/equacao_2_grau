0ea37788bbf2f2b5100e3befa078a362bf2d561e
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-14 20:34:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,55 @@ print("A fórmula de BASKARA é: A(DELTA) = B² - 4ac")
 print("-----------------------------------------------")
 print("Passando suas informações para a fórmula, temos:")
 print("A(DELTA) = %.2f² - 4 x %.2f x %.2f"%(valor_b,valor_a,valor_c))
+print("")
 
-delta = (valor_b**2) - (4 * valor_a * valor_c) # declarando delta
+delta = (valor_b**2) - (4 * valor_a * valor_c) # DECLARANDO DELTA
+
+print("O valor de Delta é: %.2f"%delta)
+print("")
+
+# CONDICONIAS: 
+if delta > 0:
+
+    print("Delta é maior que 0, então o valor de X1 e X2 será diferente: ") 
+    print("")
+
+    # CAUCULANDO O VALOR DE X1 E X2 
+    print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+    print("")
+
+    # X1 ->
+    x1 = float((-valor_b + (sqrt(delta)))/(2*valor_a))
+    print("O valor de X1 é = %.2f"%x1)
+
+    # X2 ->
+    x2 = float((-valor_b - (sqrt(delta)))/(2*valor_a))
+    print("O valor de X2 é = %.2f"%x2)
+    print("")
+
+elif delta == 0:
+    print("Delta é igual a 0, então X1 e X2 serão iguais")
+    print("")
+   
+    # CAUCULANDO O VALOR DE X1 E X2   
+    print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+    print("")
+
+    # X1 ->
+    x1 = float((-valor_b + (sqrt(delta)))/(2*valor_a))
+    print(f"O valor de X1 é = {x1}")
+
+    # X2 ->
+    x2 = float((-valor_b - (sqrt(delta)))/(2*valor_a))
+    print(f"O valor de X2 é = {x2}")
+    print("")
+
+
+else:
+    print("Delta é < 0 (NEGATIVO)")
+    print("NÃO EXISTE RAIZ PARA DELTA NEGATIVO!")
+    print("")
+
+
+print("FIM DO PROGRAMA") 
 

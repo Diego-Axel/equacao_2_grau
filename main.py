005f7ea8d29a5469d4fe31233936f735a9469bab
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-14 20:11:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+############################################################
+######   PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU    ######
+######                   DIÊGO AXEL                   ######
+######                    CAICÓ-RM                    ######
+############################################################
 
 
 from math import sqrt # Importando a biblioteca math, em específico o SQRT para a dar a raiz quadrada
@@ -7,5 +11,11 @@ print("-----------------------------------------------")
 print("|  PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU  |" )
 print("-----------------------------------------------")
 print("") # espaços
+
+# PEDINDO OS VALORES QUE SERÃO ATRIBUIDOS DE: A, B E C ->
+valor_a = float(input("Digite o valor de A: "))  
+valor_b = float(input("Digite o valor de B: "))
+valor_c = float(input("Digite o valor de C: "))
+print("")
 
 

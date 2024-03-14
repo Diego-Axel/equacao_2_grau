fd9d1dc303309fb064d1990d31a2083cb066d757
Diego-Axel <diegoaxelbsr@gmail.com>
2024-03-14 20:21:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,13 @@ print("")
 
 # EQUAÇÃO APÓS DADO OS VALORES DE A, B E C:
 print("Sua equação é %.2fx² %.2fx  %.2f = 0"%(valor_a,valor_b,valor_c)) 
+print("")
+
+# CAUCULANDO O VALOR DE DELTA(A)
+print("A fórmula de BASKARA é: A(DELTA) = B² - 4ac")
+print("-----------------------------------------------")
+print("Passando suas informações para a fórmula, temos:")
+print("A(DELTA) = %.2f² - 4 x %.2f x %.2f"%(valor_b,valor_a,valor_c))
+
+delta = (valor_b**2) - (4 * valor_a * valor_c) # declarando delta
 

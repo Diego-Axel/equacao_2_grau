2a157da7e77c68638d6ee1e5482f1783b9a5f316
Diego-Axel <diegoaxelbsr@gmail.com>
2024-07-08 16:24:45 -0300

    Ajeitado para apresentação na aula de matemática
    Atualizando o meu Código para Apresentar na aula de Almir.

---
+++ interface.py
@@ -0,0 +1,13 @@
+import os
+
+def titulo():
+    os.system('clear || cls') # se for Linux use 'clear' se for Windows use 'cls'
+    print("-----------------------------------------------")
+    print("|  PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU  |")
+    print("-----------------------------------------------")
+
+
+def confirmacao():
+    print("-------------------------------------------")
+    print("| AINDA DESEJA UTILIZAR O PROGRAMA (S/N)? |")
+    print("-------------------------------------------")
--- main.py
+++ main.py
@@ -1,76 +1,107 @@
 ############################################################
 ######   PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU    ######
 ######                   DIÊGO AXEL                   ######
-######                    CAICÓ-RM                    ######
+######                    CAICÓ-RN                    ######
 ############################################################
 
 
 from math import sqrt # Importando a biblioteca math, em específico o SQRT para a dar a raiz quadrada
+import interface
 
-print("-----------------------------------------------")
-print("|  PROGRAMA RESOLVEDOR DE EQUAÇÃO DO 2º GRAU  |" )
-print("-----------------------------------------------")
-print() # espaços
-
-# PEDINDO OS VALORES QUE SERÃO ATRIBUIDOS DE: A, B E C ->
-valor_a = float(input("Digite o valor de A: "))  
-valor_b = float(input("Digite o valor de B: "))
-valor_c = float(input("Digite o valor de C: "))
-print()
-
-# EQUAÇÃO APÓS DADO OS VALORES DE A, B E C:
-print("Sua equação é %.2fx² %.2fx  %.2f = 0"%(valor_a,valor_b,valor_c)) 
-print()
-
-# CAUCULANDO O VALOR DE DELTA(A)
-print("A fórmula de BASKARA é: A(DELTA) = B² - 4ac")
-print("-----------------------------------------------")
-print("Passando suas informações para a fórmula, temos:")
-print("A(DELTA) = %.2f² - 4 x %.2f x %.2f"%(valor_b,valor_a,valor_c))
-print()
-
-delta = (valor_b**2) - (4 * valor_a * valor_c) # DECLARANDO DELTA
-
-print("O valor de Delta é: %.2f"%delta)
-print()
-
-# CONDICONIAS: 
-if delta > 0:
-    print("Delta é maior que 0, então o valor de X1 e X2 serão diferente: ") 
+resp = True
+while resp:
+    interface.titulo()
+    print()
+    # PEDINDO OS VALORES QUE SERÃO ATRIBUIDOS DE: A, B E C ->
+    verificador = True
+    while verificador:
+        try:
+            valor_a = float(input("--- Digite o valor de A: "))  
+            print()
+            verificador = False
+        except ValueError:
+            print("Por favor, coloque um número! Tente novamente.")
+    verificador = True
+    while verificador:
+        try:
+            valor_b = float(input("--- Digite o valor de B: "))
+            print()
+            verificador = False
+        except ValueError:
+            print("Por favor, coloque um número! Tente novamente.")
+    verificador = True
+    while verificador:
+        try:
+            valor_c = float(input("--- Digite o valor de C: "))
+            print()
+            verificador = False
+        except ValueError:
+            print("Por favor, coloque um número! Tente novamente.")
     print()
 
-    # CAUCULANDO O VALOR DE X1 E X2 
-    print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+    interface.titulo()
+    print()
+    print(f"A: {valor_a} | B: {valor_b} | C: {valor_c}")
+    print()
+    # EQUAÇÃO APÓS DADO OS VALORES DE A, B E C:
+    print(f"Sua equação é {valor_a}x² {valor_b}x {valor_c} = 0") 
     print()
 
-    # X1 ->
-    x1 = float((-valor_b + (sqrt(delta)))/(2*valor_a))
-    print("O valor de X1 é = %.2f"%x1)
-
-    # X2 ->
-    x2 = float((-valor_b - (sqrt(delta)))/(2*valor_a))
-    print("O valor de X2 é = %.2f"%x2)
-    print()
-elif delta == 0:
-    print("Delta é igual a 0, então X1 e X2 serão iguais")
-    print()
-   
-    # CAUCULANDO O VALOR DE X1 E X2   
-    print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+    # CAUCULANDO O VALOR DE DELTA(A)
+    print("A fórmula de BASKARA é: A(DELTA) = B² - 4ac")
+    print("-----------------------------------------------")
+    print("Passando suas informações para a fórmula, temos:")
+    print(f"A(DELTA) = {valor_b}² - 4 x {valor_a} x {valor_c}")
     print()
 
-    # X1 ->
-    x1 = float((-valor_b + (sqrt(delta)))/(2*valor_a))
-    print(f"O valor de X1 é = {x1}")
+    delta = (valor_b**2) - (4 * valor_a * valor_c) # DECLARANDO DELTA
 
-    # X2 ->
-    x2 = float((-valor_b - (sqrt(delta)))/(2*valor_a))
-    print(f"O valor de X2 é = {x2}")
+    print(f"O valor de Delta é: {delta}")
     print()
-else:
-    print("Delta é < 0 (NEGATIVO)")
-    print("NÃO EXISTE RAIZ PARA DELTA NEGATIVO!")
-    print()
+
+    # CONDICONIAS: 
+    if delta > 0:
+        print("Delta é maior que 0, então o valor de X1 e X2 serão diferente: ") 
+        print()
+
+        # CAUCULANDO O VALOR DE X1 E X2 
+        print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+        print()
+
+        # X1 ->
+        x1 = (-valor_b + (sqrt(delta)))/(2*valor_a)
+        print(f"O valor de X1 é = {x1}")
+
+        # X2 ->
+        x2 = (-valor_b - (sqrt(delta)))/(2*valor_a)
+        print(f"O valor de X2 é = {x2}")
+        print()
+    elif delta == 0:
+        print("Delta é igual a 0, então X1 e X2 serão iguais")
+        print()
     
-print("FIM DO PROGRAMA") 
-# END
+        # CAUCULANDO O VALOR DE X1 E X2   
+        print("Agora iremos achar o valor de X1 e X2. A formula para chegar no valor deles é: X = -b +- RA/2a. X1 sera atriuido o sinal +(positivo) e x2 o sinal -(negativo)")
+        print()
+
+        # X1 ->
+        x1 = (-valor_b + (sqrt(delta)))/(2*valor_a)
+        print(f"O valor de X1 é = {x1}")
+
+        # X2 ->
+        x2 = (-valor_b - (sqrt(delta)))/(2*valor_a)
+        print(f"O valor de X2 é = {x2}")
+        print()
+    else:
+        print("Delta é < 0 (NEGATIVO)")
+        print("NÃO EXISTE RAIZ PARA DELTA NEGATIVO!")
+        print()
+    interface.confirmacao()
+    resp = input("Digite sua resposta: ")
+    resp = resp.upper()
+    if (resp == "SIM") or (resp == "S"):
+        print()
+    else:
+        resp = False
+        print()    
+print("FIM DO PROGRAMA, ATÉ BREVE.") 
